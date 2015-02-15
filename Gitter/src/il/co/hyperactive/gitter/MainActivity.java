d88eb6d62390cb325ba98d1b22e8caafba3a450d
Shai <Shai11222@gmail.com>
2015-02-15 15:32:13 +0200

    First Gitter

---
+++ Gitter/src/il/co/hyperactive/gitter/MainActivity.java
@@ -0,0 +1,49 @@
+package il.co.hyperactive.gitter;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Button;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final RadioButton bt1 = (RadioButton)findViewById(R.id.radioButton1);
+        final RadioButton bt2 = (RadioButton)findViewById(R.id.radioButton2);
+        ((Button)findViewById(R.id.button1)).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				bt1.setChecked(true);
+				bt2.setChecked(true);
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
